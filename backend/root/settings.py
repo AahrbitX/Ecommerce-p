5e6326b7ac3b0f40d649528d361dfebb8c7c5745
Maneesh63 <maneeshmaneesh391@gmail.com>
2024-11-30 00:10:47 +0530

    email else

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -150,11 +150,10 @@ SIMPLE_JWT = {
     'USER_ID_CLAIM': 'user_id',
      
 }
-
-EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))  # Default to 587, commonly used for TLS
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() in ["true", "1", "yes"]  # Converts to a boolean
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "your_default_email@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "your_default_password")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "your_default_email@gmail.com")
